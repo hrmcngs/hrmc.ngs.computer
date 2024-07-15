359b29b706e7aad33d3a3eb5f12b159341aff6d7
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2024-07-15 13:23:56 +0900

    test sw

---
+++ sw.js
@@ -0,0 +1,23 @@
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    'mako5656.github.io/pwa/',
+    'mako5656.github.io/pwa/app.js',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches
+        .open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches
+        .match(event.request)
+        .then(function(response) {
+            return response ? response : fetch(event.request);
+        })
+    );
+});
